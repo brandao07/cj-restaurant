095d30422bd444a65dd43a0e606c4bcef4886628
André Brandão <75815203+brandao07@users.noreply.github.com>
2025-05-23 22:44:21 +0100

    added 04_seed.sql

---
+++ customer-service/db/04_seed.sql
@@ -0,0 +1,13 @@
+-- Restaurant tables
+INSERT INTO restaurant_tables DEFAULT VALUES;
+INSERT INTO restaurant_tables DEFAULT VALUES;
+INSERT INTO restaurant_tables DEFAULT VALUES;
+INSERT INTO restaurant_tables DEFAULT VALUES;
+INSERT INTO restaurant_tables DEFAULT VALUES;
+INSERT INTO restaurant_tables DEFAULT VALUES;
+INSERT INTO restaurant_tables DEFAULT VALUES;
+INSERT INTO restaurant_tables DEFAULT VALUES;
+INSERT INTO restaurant_tables DEFAULT VALUES;
+INSERT INTO restaurant_tables DEFAULT VALUES;
+
+-- No customers to insert do later..
